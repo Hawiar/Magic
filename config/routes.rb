4d32306d36fccf318c1e9011f4579f7fccf20388
Hawiar <hawiar6@gmail.com>
2015-04-20 12:48:29 -0400

    Config

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+   get '/conways'=>'life#conways'  
   # You can have the root of your site routed with "root"
    root 'players#index'
 
